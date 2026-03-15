53798cf3972962e938fad6863dcb278c2af5c32d
IvanBanal <ibanal@rrc.ca>
2026-03-15 17:38:20 -0500

    Adding getLoanById function for loanController.ts.

--- src/api/v1/controllers/loanController.ts
+++ src/api/v1/controllers/loanController.ts
@@ -15,5 +15,28 @@ export const getAllLoans = (req: Request, res: Response) => {
     });
 };
 
+/**
+ * Get a single loan application by ID.
+ */
+export const getLoanById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const data = loanService.getLoanById(id);
+
+    if (!data) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({
+            success: false,
+            message: "Loan application not found"
+        });
+        return;
+    }
+    res.status(HTTP_STATUS.OK).json({
+        succces: true,
+        data
+    });
+};
+
+
+
+
 
 
