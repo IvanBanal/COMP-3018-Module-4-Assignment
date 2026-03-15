385eccd65dc9a372e7e5ba392ea592236af93604
IvanBanal <ibanal@rrc.ca>
2026-03-15 16:47:33 -0500

    Adding getLoanById function for loanService.ts.

--- src/api/v1/services/loanService.ts
+++ src/api/v1/services/loanService.ts
@@ -7,3 +7,15 @@ import { loans } from "../data/loanData";
  */
 export const getAllLoans = (): LoanApplication[] => loans;
 
+/**
+ * Get a loan application by its ID.
+ * @param {number} id - The ID of the loan to retrieve.
+ * @throws {Error} Throws an error if the loan is not found.
+ * @returns {LoanApplication} The loan with the specified ID.
+ */
+export const getLoanById = (id: number): LoanApplication => {
+    const loan = loans.find((loan) => loan.id === id);
+
+    if (!loan) throw new Error("Loan application not found");
+    return loan;
+};
