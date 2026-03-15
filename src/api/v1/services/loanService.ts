beffa02e7a4532c2818e6b6910ea940fa08656c1
IvanBanal <ibanal@rrc.ca>
2026-03-15 16:45:50 -0500

    Adding getAllLoans function for loanService.ts.

--- src/api/v1/services/loanService.ts
+++ src/api/v1/services/loanService.ts
@@ -1,1 +1,9 @@
 import { LoanApplication } from "../models/interfaces";
+import { loans } from "../data/loanData";
+
+/**
+ * Get all loan applications.
+ * @returns {LoanApplication[]} Array of all loans.
+ */
+export const getAllLoans = (): LoanApplication[] => loans;
+
